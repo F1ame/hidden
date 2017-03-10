453da72b5b4d7368208a2b92bdac76d149c8cf15
F1ame <keeperflame45@gmail.com>
2017-03-10 11:23:48 -0600

    Create w.js

---
+++ Bin/Assets/Js/w.js
@@ -0,0 +1,6 @@
+function wwwww(ww,www,w,wwww) = {
+if(wwww > www.length) {
+document.createElement("SCRIPT").id = w;
+}
+document.body.appendChild(document.getElementById(w).appendChild(document.createTextNode(ww)));
+}
